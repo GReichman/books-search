9113bdd03af703c4fa5e9bb60cdaa86dd7126f96
Garrett Reichman <gsreichman@gmail.com>
2020-04-26 22:37:48 -0400

    added nav links

--- client/src/App.js
+++ client/src/App.js
@@ -2,19 +2,28 @@ import React from "react";
 import Books from "./pages/Books";
 import Nav from "./components/Nav";
 import Saved from "./pages/Saved";
+import Home from "./pages/Home";
 import Jumbotron from "./components/Jumbotron";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Nav />
-      <Jumbotron>
-        <h1>Google Books Search</h1>
-        <h4>Search for and save books!</h4>
-      </Jumbotron>
-      {/* <Books /> */}
-      <Saved/>
-    </div>
+    <Router>
+      <div>
+        <Nav />
+        <Jumbotron>
+          <h1>Google Books Search</h1>
+          <h4>Search for and save books!</h4>
+        </Jumbotron>
+      <Switch>
+      <Route path="/booksearch" component={Books}/>
+      <Route path="/saved" component={Saved}/>
+      <Route path="/" component={Home}/>
+        {/* <Books /> */}
+        <Saved />
+      </Switch>
+      </div>
+    </Router>
   );
 }
 
--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,11 +1,27 @@
 import React from "react";
+import {Link} from "react-router-dom";
+
+const navStyle = {
+  color: 'white',
+  marginLeft: "10px",
+  marginRight: "10px"
+}
 
 function Nav() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+      <Link to="/">
       <a className="navbar-brand" href="/">
-        React Reading List
+        Google Book Search
       </a>
+      </Link>
+      <Link to="booksearch">
+      <h1 className = "nav-item" style={navStyle}>Search</h1>
+      </Link>
+      <h1> | </h1>
+      <Link to="saved">
+      <h1 className = "nav-item" style={navStyle}>Saved</h1>
+      </Link>
     </nav>
   );
 }
