84c5642eaa090070dab91f5eb37a4e7d73aee512
Garrett Reichman <gsreichman@gmail.com>
2020-04-28 00:54:48 -0400

    linking heroku

---
+++ client/build/precache-manifest.d2d577839475e0aee016c2ee6f99f5d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ff8d8af60acf26b3d4c",
+    "url": "/static/css/main.8a76a5d4.chunk.css"
+  },
+  {
+    "revision": "6ff8d8af60acf26b3d4c",
+    "url": "/static/js/main.cacae3c1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ad1d2404ee0a446142f9",
+    "url": "/static/js/2.d93b2b37.chunk.js"
+  },
+  {
+    "revision": "005fc2caf695ae68dcb19ae181782f24",
+    "url": "/index.html"
+  }
+];
